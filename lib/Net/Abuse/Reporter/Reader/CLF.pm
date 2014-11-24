c37325ce28bdec4d0908925f843ce937037f6ecf
Paul Bennett <paul.w.bennett@gmail.com>
2014-11-24 07:14:30 -0500

    that's not how multilog works, at least

--- lib/Net/Abuse/Reporter/Reader/CLF.pm
+++ lib/Net/Abuse/Reporter/Reader/CLF.pm
@@ -15,27 +15,28 @@ our $CLF_FORMAT = DateTime::Format::Strptime->new(
 
 sub parse {
     # 127.0.0.1 user-identifier frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326
-    # Also allow optional TAI64(N(A)?)? stamp at the start, because some log formats can get funky like that
+    # Also allow optional multilog-style TAI64(N(A)?)? stamp at the start
     my $self = shift;
     my ($line) = @_;
     my %rv;
-    if ($line =~ /^\@?(.{8}|.{12}|.{16})\s/io) {
-        $rv{ tai64 } = tai2unix($1);
-        $line =~ s/^\@?$rv{tai64}//;
+    if ($line =~ /^\@([0-9a-f]{16}|[0-9a-f]{24}|[0-9a-f]{32})/io) {
+        my $tai = $1;
+        $rv{ tai64 } = pack 'N*', map { hex $_ } split /(..)/, $tai;
+        $line =~ s/^\@$tai//;
     }
     if ($line =~ qr/^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s(\S+)\s(\S+)\s\[([^\]]+)\]\s("[^"]+")\s(\d+)\s(\d+)/io) {
-        @rv{qw( ip identity user dt req code size ) } = ($1, $2, $3, $4, $5, $6, $7);
+        @rv{qw( ip identity user clf_dt req code size ) } = ($1, $2, $3, $4, $5, $6, $7);
     }
     elsif ($line =~ qr/^([0-9a-f:]{2,})\s(\S+)\s(\S+)\s\[([^\]]+)\]\s("[^"]+")\s(\d+)\s(\d+)/io) {
-        @rv{qw( ip identity user dt req code size ) } = ($1, $2, $3, $4, $5, $6, $7);
+        @rv{qw( ip identity user clf_dt req code size ) } = ($1, $2, $3, $4, $5, $6, $7);
     }
     return \%rv;
 }
 
 sub _normalize_special {
     my $rv = shift;
-    $rv->{ incident_time } = $CLF_FORMAT->parse_datetime(delete $rv->{ dt })->epoch;
-    $rv->{ detection_time } = $rv->{ tai64 } ? delete $rv->{ tai64 } : $rv->{ incident_time };
+    $rv->{ incident_time } = $CLF_FORMAT->parse_datetime(delete $rv->{ clf_dt })->epoch;
+    $rv->{ detection_time } = $rv->{ tai64 } ? tai2unix(delete $rv->{ tai64 }) : $rv->{ incident_time };
     return $rv;
 }
 
